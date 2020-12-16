6ec32b2b38c5aa277c259d177f59e81bc2e0691f
Athavan Karunakaran <athavan.karunakaran@gmail.com>
2020-12-15 22:27:56 -0500

    Converted eslintrc to single quotes

--- personal/.eslintrc.js
+++ personal/.eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "extends": [
-        "airbnb-base",
-        "plugin:react/recommended",
-        "plugin:cypress/recommended"
+    'extends': [
+        'airbnb-base',
+        'plugin:react/recommended',
+        'plugin:cypress/recommended'
     ],
-    "parser": "babel-eslint",
-    "rules": {
-        "indent": ["error", 4],
-        "comma-dangle": ["error", "never"],
-        "quotes": ["error", "single", { "avoidEscape": true }],
-        "jsx-quotes": ["error", "prefer-single"],
-        "react/jsx-no-literals": ["error"],
-        "react/jsx-indent" : ["error", 4],
-        "react/jsx-indent-props": ["error", 4],
-        "max-lines": ["error", 80],
-        "global-require": "off",
-        "import/no-dynamic-require": "off"
+    'parser': 'babel-eslint',
+    'rules': {
+        'indent': ['error', 4],
+        'comma-dangle': ['error', 'never'],
+        'quotes': ['error', 'single', { 'avoidEscape': true }],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'react/jsx-no-literals': ['error'],
+        'react/jsx-indent' : ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'max-lines': ['error', 80],
+        'global-require': 'off',
+        'import/no-dynamic-require': 'off'
     },
-    "env": {
-        "browser": true,
-        "jest": true
+    'env': {
+        'browser': true,
+        'jest': true
     }
 };
