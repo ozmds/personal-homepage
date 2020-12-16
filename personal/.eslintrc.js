a65c23a06efd9c298fb6fb31dcf8ecf852b7a994
Athavan Karunakaran <athavan.karunakaran@gmail.com>
2020-12-15 23:01:56 -0500

    Eslint Fixes

--- personal/.eslintrc.js
+++ personal/.eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    'extends': [
+    extends: [
         'airbnb-base',
         'plugin:react/recommended',
         'plugin:cypress/recommended'
     ],
-    'parser': 'babel-eslint',
-    'ignorePatterns': ['src/serviceWorker.js', 'build/**/*.js'],
-    'rules': {
-        'indent': ['error', 4],
+    parser: 'babel-eslint',
+    ignorePatterns: ['src/serviceWorker.js', 'build/**/*.js'],
+    rules: {
+        indent: ['error', 4],
         'comma-dangle': ['error', 'never'],
-        'quotes': ['error', 'single', { 'avoidEscape': true }],
+        quotes: ['error', 'single', { 'avoidEscape': true }],
         'jsx-quotes': ['error', 'prefer-single'],
         'react/jsx-no-literals': ['error'],
-        'react/jsx-indent' : ['error', 4],
+        'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props': ['error', 4],
-        'max-lines': ['error', 80],
+        'max-lines': ['warn', 80],
         'global-require': 'off',
         'import/no-dynamic-require': 'off'
     },
-    'env': {
-        'browser': true,
-        'jest': true
+    env: {
+        browser: true,
+        jest: true
     }
 };
